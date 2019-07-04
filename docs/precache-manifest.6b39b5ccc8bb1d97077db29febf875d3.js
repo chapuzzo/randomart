2b93ff1c0b8fbeddfa118b4c2183804bb60392b4
chapuzzo <luismax@gmail.com>
2019-07-04 20:09:49 +0200

    built assets

---
+++ docs/precache-manifest.6b39b5ccc8bb1d97077db29febf875d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "234ffe5e2a52e1cb69c4",
+    "url": "js/chunk-vendors.8ef14dfa.js"
+  },
+  {
+    "revision": "7477ce06934231d6b328",
+    "url": "js/app.877fabc3.js"
+  },
+  {
+    "revision": "8809554ce63e01a313a0e0e18fc1f429",
+    "url": "index.html"
+  },
+  {
+    "revision": "7477ce06934231d6b328",
+    "url": "css/app.7d590d38.css"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.febe6d7245d315b618b746284eed9cdc.js"
+  "precache-manifest.6b39b5ccc8bb1d97077db29febf875d3.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "randomart"});
