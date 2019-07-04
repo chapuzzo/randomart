e2a98f717bbd425436029aade3b597c6f96ba463
chapuzzo <luismax@gmail.com>
2019-07-04 15:24:25 +0200

    changed boring white background

--- src/App.vue
+++ src/App.vue
@@ -10,12 +10,20 @@
 </template>
 
 <style lang="scss">
+  body {
+    background-color: ghostwhite;
+  }
+
   #app {
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
+    max-width: 1000px;
+    margin: 0 auto;
+    padding: 15px;
+    border: 1px gray dashed;
   }
 
   #nav {
@@ -27,6 +35,23 @@
 
       &.router-link-exact-active {
         color: #42b983;
+      }
+    }
+  }
+
+  .controls {
+    margin-left: auto;
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: flex-end;
+    >input, fieldset {
+      width: 25%;
+      label {
+        display: flex;
+        justify-content: flex-end;
+        > input[type="color"] {
+          margin-left:  6px;
+        }
       }
     }
   }
