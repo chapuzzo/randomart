80aebed304791adf5dc0a68ba798b850bcc9f0a3
chapuzzo <luismax@gmail.com>
2019-07-04 15:25:55 +0200

    created basic ui to test things

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,10 +8,60 @@
     <TrianglesDisplay :cell-size="Number(cellSize)" :seed="seed"/>
 
     <div class="controls">
-      <input v-model="roughness" type="range" min="0" max="12" step="0.5">
-      <input v-model="color" type="color">
+      <fieldset><legend>roughness</legend>
+        <label>roughness
+          <input v-model.number="roughness" type="range" min="0" max="12" step="0.5">
+        </label>
+        <label>bowing
+          <input v-model.number="bowing" type="range" min="0" max="12" step="0.5">
+        </label>
+      </fieldset>
+      <fieldset>
+        <legend>border</legend>
+        <label>color
+          <input v-model="strokeColor" type="color">
+        </label>
+        <label>width
+          <input v-model.number="strokeWidth" type="range" min="0" max="12" step="0.5">
+        </label>
+
+      </fieldset>
+      <fieldset>
+        <legend>fill</legend>
+        <label>weight
+          <input v-model.number="fillWeight" type="range" min="0" max="20" step="0.5">
+        </label>
+        <label>gap
+          <input v-model.number="hachureGap" type="range" min="0" max="20" step="0.5">
+        </label>
+        <label>angle
+          <input v-model.number="hachureAngle" type="range" min="0" max="360" step="0.5">
+        </label>
+        <label>style
+          <select v-model="fillStyle">
+            <option value="hachure">hachure</option>
+            <option value="solid">solid</option>
+            <option value="zigzag">zigzag</option>
+            <option value="cross-hatch">cross-hatch</option>
+            <option value="dots">dots</option>
+            <option value="sunburst">sunburst</option>
+            <option value="dashed">dashed</option>
+            <option value="zigzag-line">zigzag-line</option>
+          </select>
+        </label>
+      </fieldset>
+
     </div>
-    <RoughDisplay :color="color" :roughness="Number(roughness)"/>
+    <RoughDisplay
+      :roughness="roughness"
+      :bowing="bowing"
+      :stroke-color="strokeColor"
+      :stroke-width="strokeWidth"
+      :fill-weight="fillWeight"
+      :hachure-angle="hachureAngle"
+      :hachure-gap="hachureGap"
+      :fill-style="fillStyle"
+    />
   </div>
 </template>
 
@@ -25,9 +75,15 @@ export default {
   data () {
     return {
       seed: getSeed(),
+      bowing: 1,
       cellSize: 20,
-      color: '#ff0000',
-      roughness: 1
+      strokeColor: '#ff0000',
+      roughness: 2,
+      strokeWidth: 1,
+      fillWeight: 2,
+      hachureAngle: 45,
+      hachureGap: 5,
+      fillStyle: 'hachure'
     }
   },
   components: {
@@ -43,9 +99,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .controls {
-    margin-left: auto;
-    display: flex;
-    justify-content: flex-end;
-  }
+
 </style>
