b72358a8fff3fcab5b59fed4382785a3aa65361b
chapuzzo <luismax@gmail.com>
2019-07-04 15:59:50 +0200

    built assets

---
+++ docs/precache-manifest.3c710033142da6a27c2fd55a049f886b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "3075648b339aa3dfb6fd",
+    "url": "js/chunk-vendors.9e1a0af7.js"
+  },
+  {
+    "revision": "7adf164f6b03e61b010f",
+    "url": "js/app.0a95f151.js"
+  },
+  {
+    "revision": "2c3fbf14530431785b1464b378fd0c71",
+    "url": "index.html"
+  },
+  {
+    "revision": "7adf164f6b03e61b010f",
+    "url": "css/app.13774afe.css"
+  }
+];
