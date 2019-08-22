795beacaf32cd3acd68e362981958332f704fa18
chapuzzo <luismax@gmail.com>
2019-08-22 20:50:43 +0200

    rebuilt assets

---
+++ docs/precache-manifest.e33ee7a411ec8b9437bbda569f21434a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "cc584a5be22d88c56d32",
+    "url": "js/chunk-vendors.b57fd9d5.js"
+  },
+  {
+    "revision": "751ddb8e6d91f391b0a9",
+    "url": "js/app.d417e3a5.js"
+  },
+  {
+    "revision": "f1733d18440e1abb310ccb0145bc0e77",
+    "url": "index.html"
+  },
+  {
+    "revision": "cc584a5be22d88c56d32",
+    "url": "css/chunk-vendors.402cc6b7.css"
+  },
+  {
+    "revision": "751ddb8e6d91f391b0a9",
+    "url": "css/app.30a2bfa4.css"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.eac3046829af3d4fbb6be83cf8c4b071.js"
+  "precache-manifest.e33ee7a411ec8b9437bbda569f21434a.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "randomart"});
