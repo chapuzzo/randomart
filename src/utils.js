4f9688758c724ba7ed0552d18e9d501548439e97
chapuzzo <luismax@gmail.com>
2019-09-21 15:54:08 +0200

    added shuffle to toolbelt

--- src/utils.js
+++ src/utils.js
@@ -132,3 +132,12 @@ export const lightenPalette = (palette, amount = 0.5) => palette.map(color => {
 
   return lightColor
 })
+
+export const shuffle = array => {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]]
+  }
+
+  return array
+}
